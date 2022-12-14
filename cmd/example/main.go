747cc21001dc26b8a9f31585d98377baff03c033
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-12-14 13:48:22 +0200

    added comments to search script

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -32,6 +32,7 @@ func main() {
 		panic(err)
 	}
 
+	// Showing names to id
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter text: ")
@@ -43,17 +44,20 @@ func main() {
 			fmt.Printf("article %s is id %d\n", text[:len(text)-1], val)
 		}
 	}
+	// Reading two ids
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter two article ids:")
+	fmt.Print("Enter two article ids separated by a space:")
 	text, _ := reader.ReadString('\n')
 	ids := strings.Split(text, " ")
 	id1, _ := strconv.ParseUint(ids[0], 10, 32)
 	id2, _ := strconv.ParseUint(ids[1][:len(ids[1])-1], 10, 32)
 	fmt.Println(id1, id2)
+	// Searching
 	res, err := sc.ShortestPath(uint32(id1), uint32(id2), func(i int) {})
 	if err != nil {
 		panic(err)
 	}
+	// Converting ids to names
 	for i := range res {
 		v, err := db.GetName(res[i])
 		if err != nil {
