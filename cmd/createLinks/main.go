318d94fe5f0439dfa4927f05cb889ed0f52c72d6
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-12-14 14:55:30 +0200

    changed createLinks script to use goroutines

--- cmd/createLinks/main.go
+++ cmd/createLinks/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/schollz/progressbar/v3"
 
 	"github.com/sha65536/wikilink/linkapi"
 )
+
+const NUMWORKERS = 10
 
 func main() {
 	// Creating db handler
@@ -29,11 +33,38 @@ func main() {
 	sc := linkapi.MakeScrapeHandler(db)
 	bar := progressbar.Default(int64(len(arts)))
 
+	// Creating workers
+	var wg sync.WaitGroup
+	var artChan = make(chan Art)
+	for i := 0; i < NUMWORKERS; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			downloadWorker(sc, artChan, arts)
+		}()
+	}
+
 	// Looping over all the articles
 	for k, v := range arts {
 		bar.Add(1)
 		// Getting all links from each
-		err = sc.GetAllLinks("he", k, func(s []string) {
+		artChan <- Art{Name: k, Id: v}
+	}
+
+	// Waiting for the last ones to finish
+	close(artChan)
+	wg.Wait()
+}
+
+type Art struct {
+	Name string
+	Id   uint32
+}
+
+func downloadWorker(sc *linkapi.ScrapeHandler, in chan Art, arts map[string]uint32) {
+	for val := range in {
+		// Getting all links
+		sc.GetAllLinks("he", val.Name, func(s []string) {
 			var res = make([]uint32, 0, len(s))
 			for i := range s {
 				if id, ok := arts[s[i]]; ok {
@@ -41,12 +72,9 @@ func main() {
 				}
 			}
 			// Adding to DB
-			if err := db.AddLinks(v, res); err != nil {
+			if err := sc.DB.AddLinks(val.Id, res); err != nil {
 				panic(err)
 			}
 		})
-	}
-	if err != nil {
-		panic(err)
 	}
 }
